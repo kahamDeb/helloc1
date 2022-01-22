21ce0e4c88c210bfa599c2ce9be4e0b49770f575
kahamDeb <98123521+kahamDeb@users.noreply.github.com>
2022-01-22 23:45:03 +0530

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    printf("hello world!");
+    printf("\nthis is my first c program so it is just\n");
+    printf("print some command");
+    printf("\nand also it is not going take any kind of input");
+    printf("this is the first program\ni have ever doing\n");
+    printf("hehehehehehe hahahahahahahahah 😂😂");
+    printf("\nhehehehehehehehehehehehehehehe\nhahahahahahahahahahahahahahahahahahahahah");
+}
